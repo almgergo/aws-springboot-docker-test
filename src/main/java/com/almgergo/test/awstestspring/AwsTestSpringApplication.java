4bdc730eafaae198afe6b75026d37f029aaced45
Gergo Almasy <galmasy@chappuishalder.com>
2021-04-29 15:21:50 +0200

    Init

---
+++ src/main/java/com/almgergo/test/awstestspring/AwsTestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.almgergo.test.awstestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTestSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/almgergo/test/awstestspring/AwsTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almgergo.test.awstestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
