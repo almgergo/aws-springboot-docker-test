6158386b3d6072efa1f380874304637cfc1a64cd
Almasy Gergo <galmasy@chappuishalder.com>
2021-05-03 13:15:28 +0200

    eb deploy

--- src/main/java/com/almgergo/test/awstestspring/AwsTestSpringApplication.java
+++ src/main/java/com/almgergo/test/awstestspring/AwsTestSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsTestSpringApplication {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(20);
+        Thread.sleep(20000);
         SpringApplication.run(AwsTestSpringApplication.class, args);
     }
 
