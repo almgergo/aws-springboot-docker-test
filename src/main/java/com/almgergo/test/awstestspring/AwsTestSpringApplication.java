4c80d0d1e42c044e76e583e076271adfc400ea69
Almasy Gergo <galmasy@chappuishalder.com>
2021-05-03 12:35:35 +0200

    Dockerrun

--- src/main/java/com/almgergo/test/awstestspring/AwsTestSpringApplication.java
+++ src/main/java/com/almgergo/test/awstestspring/AwsTestSpringApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsTestSpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(20);
         SpringApplication.run(AwsTestSpringApplication.class, args);
     }
 
